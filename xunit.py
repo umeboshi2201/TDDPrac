187403c395f203808a799da194c09a99c5052ad0
umeboshi2201 <umeume2201@gmail.com>
2020-04-02 20:32:28 +0900

    p164

--- xunit.py
+++ xunit.py
@@ -1,3 +1,7 @@
+class TestResult:
+	def summary(self):
+		return "1 run, 0 failed"
+
 class TestCase:
 	def __init__(self, name):
 		self.name = name
@@ -10,6 +14,7 @@ class TestCase:
 		method = getattr(self, self.name)
 		method()
 		self.tearDown()
+		return TestResult()
 
 class WasRun(TestCase):
 	def setUp(self):
@@ -24,5 +29,10 @@ class TestCaseTest(TestCase):
 		test = WasRun("testMethod")
 		test.run()
 		assert("setUp testMethod tearDown " == test.log)
+	def testResult(self):
+		test = WasRun("testMethod")
+		result = test.run()
+		assert("1 run, 0 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
