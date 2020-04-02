bb36b7b60eb6c87ee020b110f38e5250a284d672
umeboshi2201 <umeume2201@gmail.com>
2020-04-02 20:56:56 +0900

    p170

--- xunit.py
+++ xunit.py
@@ -1,10 +1,13 @@
 class TestResult:
 	def __init__(self):
 		self.runCount = 0
+		self.errorCount = 0
 	def testStarted(self):
 		self.runCount = self.runCount + 1
+	def testFailed(self):
+		self.errorCount = self.errorCount + 1
 	def summary(self):
-		return "%d run, 0 failed" % self.runCount
+		return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class TestCase:
 	def __init__(self, name):
@@ -45,7 +48,13 @@ class TestCaseTest(TestCase):
 		test = WasRun("testBrokenMethod")
 		result = test.run()
 		assert("1 run, 1 failed" == result.summary())
+	def testFailedResultFormatting(self):
+		result = TestResult()
+		result.testStarted()
+		result.testFailed()
+		assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
 # TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
