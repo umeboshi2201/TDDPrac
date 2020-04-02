406eb581988afd45546b3e27590cd29b3bbc065d
umeboshi2201 <umeume2201@gmail.com>
2020-04-02 18:01:32 +0900

    p146

--- xunit.py
+++ xunit.py
@@ -1,10 +1,14 @@
 class WasRun:
 	def __init__(self, name):
 		self.wasRun = None
+		self.name = name
+	def run(self):
+		method = getattr(self, self.name)
+		method()
 	def testMethod(self):
 		self.wasRun = 1
 
 test = WasRun("testMethod")
 print(test.wasRun)
-test.testMethod()
+test.run()
 print(test.wasRun)
